c4518055adc8e0580a2c5deae6a08c73e040409d
shailesh-keshri <keshailesh98@gmail.com>
2024-05-26 09:37:32 +0530

    merged conflicts resolved

--- src/app/components/super-visor/super-visor.component.ts
+++ src/app/components/super-visor/super-visor.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-super-visorr',
+  selector: 'app-super-visor',
   templateUrl: './super-visor.component.html',
   styleUrl: './super-visor.component.css'
 })
